0b7a93b3161d53c50e344db7e53f752515c322d8
Carlos_De_Los_Reyes <Carlos_De_Los_Reyes@EPMXGUAW0292>
2018-06-29 10:18:59 -0500

    New Stored procedure

---
+++ Git_StackOverflow2010/Git_StackOverflow2010/dbo.Procedure.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[USP_Posts_Users_Get]
+
+AS
+
+SELECT 
+    TOP 500
+    Users.Id as [User Link],
+    Count(Posts.Id) AS Answers,
+    CAST(AVG(CAST(Score AS float)) as numeric(6,2)) AS [Average Answer Score]
+FROM
+    Posts
+  INNER  JOIN
+    Users ON Users.Id = OwnerUserId
+WHERE 
+    PostTypeId = 2 and CommunityOwnedDate is null and ClosedDate is null
+GROUP BY
+    Users.Id, DisplayName
+HAVING
+    Count(Posts.Id) > 10
+ORDER BY
+    [Average Answer Score] DESC
+OPTION (RECOMPILE)
